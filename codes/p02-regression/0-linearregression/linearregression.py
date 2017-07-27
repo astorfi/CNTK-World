298dc1f506b3fce5504012063c047399066cc6ce
iramirsina <amirsina.torfi@gmail.com>
2017-07-26 22:43:53 -0400

    welcome section

--- codes/p02-regression/0-linearregression/linearregression.py
+++ codes/p02-regression/0-linearregression/linearregression.py
@@ -5,25 +5,32 @@ from cntk import Trainer, learning_rate_schedule, UnitType
 from cntk.learners import sgd
 from cntk.ops import *
 from cntk.layers import default_options, Dense
+import argparse
 
 #################
 ### parameter ###
 #################
 
-# data parameters
-num_samples = 500
+# Define the parser
+parser = argparse.ArgumentParser()
+parser.add_argument('-num', '--num_samples', type=int,
+                    default=500, help='number of samples')
+parser.add_argument('-lr', '--initial_learning_rate', type=float,
+                    default=0.001, help='initial learning rate')
+parser.add_argument('-num_t', '--num_samples_to_train', type=int,
+                    default=400, help='number of samples for training')
+parser.add_argument('-num_it', '--num_iterations', type=int,
+                    default=400, help='number of iterations')
+parser.add_argument('-bs', '--batch_size', type=int,
+                    default=25, help='mini batch size')
 
-# Network parameters
+args = parser.parse_args()
+
+
+# Network has only one output which is its prediction.
 input_dim = 1
 num_outputs = 1
-
-# Training parameters
-initial_learning_rate = 0.001
-minibatch_size = 25
-num_samples_to_train = 400
-num_minibatches_to_train = int(num_samples_to_train / minibatch_size)
-num_iterations = 400
-
+num_minibatches_to_train = int(args.num_samples_to_train / args.batch_size)
 
 
 ###################################
@@ -31,7 +38,7 @@ num_iterations = 400
 ###################################
 
 # random data
-features = np.linspace(-1, 1, num_samples)
+features = np.linspace(-1, 1, args.num_samples)
 predictions = 2 * features + np.random.randn(*features.shape) * 0.5
 
 # Plotting the scatter plot
@@ -68,14 +75,14 @@ eval_fun = cntk.squared_error(pred, label)
 ######################
 
 # Instantiate the trainer object to drive the model training
-learning_rate = learning_rate_schedule(initial_learning_rate, UnitType.minibatch)
+learning_rate = learning_rate_schedule(args.initial_learning_rate, UnitType.minibatch)
 optimizer_op = sgd(pred.parameters, learning_rate)
 train_op = Trainer(pred, (loss, eval_fun), [optimizer_op])
 
-for step in range(0, num_iterations):
+for step in range(0, args.num_iterations):
     for batch_num in range(0, num_minibatches_to_train):
-        batch_features = features[(batch_num * minibatch_size):(batch_num * minibatch_size + minibatch_size), :]
-        batch_labels = predictions[(batch_num * minibatch_size):(batch_num * minibatch_size + minibatch_size), :]
+        batch_features = features[(batch_num * args.batch_size):(batch_num * args.batch_size + args.batch_size), :]
+        batch_labels = predictions[(batch_num * args.batch_size):(batch_num * args.batch_size + args.batch_size), :]
         train_op.train_minibatch({input: batch_features, label: batch_labels})
         training_loss = train_op.previous_minibatch_loss_average
         eval_value = train_op.previous_minibatch_evaluation_average
@@ -87,12 +94,12 @@ for step in range(0, num_iterations):
 ##############################
 
 # Test data
-test_features = features[num_samples_to_train:num_samples, :]
-test_labels = predictions[num_samples_to_train:num_samples, :]
+test_features = features[args.num_samples_to_train:args.num_samples, :]
+test_labels = predictions[args.num_samples_to_train:args.num_samples, :]
 
 # Train data
-train_features = features[0:num_samples_to_train, :]
-train_labels = predictions[0:num_samples_to_train, :]
+train_features = features[0:args.num_samples_to_train, :]
+train_labels = predictions[0:args.num_samples_to_train, :]
 
 # Print out our weight and bias
 print("Trained parameters are: w= {0:.2f}, b={1:.2f}".format(pred.W.value[0][0], pred.b.value[0]))
